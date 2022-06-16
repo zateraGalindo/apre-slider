9678fba4f8df10f47f8b0bb906048ba552d65113
Gabriel Galindo <zatera_@hotmail.com>
2022-06-15 22:53:09 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Slider
+
+Aprendemos a fazer um slider responsivo 
+
+Usamos as informações passadas nas auluas de Javascript
---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+let totalSlider = document.querySelectorAll('.slider--item').length;
+let currentSlider = 0;
+
+let sliderWidth = document.querySelector('.slider').clientWidth;
+
+document.querySelector('.slider--width').style.width = `${sliderWidth * totalSlider}px`;
+
+document.querySelector('.slider--controls').style.width = `${sliderWidth}px`;
+
+document.querySelector('.slider--controls').style.height = `${document.querySelector('.slider').clientHeight}px`;
+
+
+function goPrev() {
+    currentSlider--;
+    if(currentSlider < 0) {
+        currentSlider = totalSlider - 1;
+    }
+    updaterMargin();
+}
+
+function goNext() {
+    currentSlider++;
+    if(currentSlider > (totalSlider-1)){
+        currentSlider = 0;
+    }
+    updaterMargin();
+}
+
+function updaterMargin() {
+    let sliderItemWidth = document.querySelector('.slider--item').clientWidth;
+    let newMargin = (currentSlider * sliderItemWidth);
+    document.querySelector('.slider--width').style.marginLeft = `-${newMargin}px`;
+}
+
+setInterval(goNext, 5000);
